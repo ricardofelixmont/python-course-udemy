49caae3709614323558bfa04b50bb5bfa131a8fc
Ricardo Felix <ricardofelix_mont@outlook.com>
2019-01-04 00:27:32 -0200

    Modulos em python

--- 6-files/exercicio_operacoes_com_adicao/app.py
+++ 6-files/exercicio_operacoes_com_adicao/app.py
@@ -10,6 +10,18 @@ from addition import Addition
 # - It should implement `Addition.add()`, `subtract()`, `multiply()` and `divide()` methods.
 # - It cannot use addition, subtraction, multiplication and division operators (`+`, `-`, `*` and `/`) directly.
 #   Instead, it should be only based on the `Addition.add()` function from the `addition` module.
+from addition import Addition
+# You don't need to change the import statement
+# now you can use Addition.add() function from the addition module like this:
+# res = Addition.add(100, 150)
+# the Addition.add() function takes in two parameters `num1` and `num2` and return the sum of `num1` and `num2`
+
+
+# Please create and implement a Calculator class, which makes use of the `addition` module.
+# Your Calculator should achieve these goals:
+# - It should implement `Addition.add()`, `subtract()`, `multiply()` and `divide()` methods.
+# - It cannot use addition, subtraction, multiplication and division operators (`+`, `-`, `*` and `/`) directly.
+#   Instead, it should be only based on the `Addition.add()` function from the `addition` module.
 # - To simplify the problem, you may expect input for the multiply() and divide() methods are all non-integers,
 #   and will always be valid, i.e. all non-negative integers and no 0 as divisor.
 
@@ -43,14 +55,17 @@ class Calculator:
     # you may assume num1 is always a non-negative integer, and num2 is always a positive integer
     @classmethod
     def divide(cls, num1, num2):
-        res = 0
+        res = num1
         count = 0
         while True:
-            res = Addition.add(num1, -num2)
+            res = Addition.add(res, -num2)
+            count = Addition.add(count, 1)
             if res == 0:
                 return count
-    
-    
+
+
+
 print(Calculator.add(2, 2))
 print(Calculator.subtract(8, 4))
 print(Calculator.multiply(2, 4))
+print(Calculator.divide(8,2))
