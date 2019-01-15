f7107fda06d5f301ef24fc9297bc1c73c8322a80
Ricardo Felix <ricardofelix_mont@outlook.com>
2019-01-15 20:06:46 -0200

    Scraping first view

---
+++ 11-web-scraping/MilesTone3/pages/quotes_page.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+from locators.quotes_page_locators import QuotesPageLocators
+from parsers.quote import QuoteParser
+
+
+
+class QuotesPage:
+    def __init__(self, page):
+        self.soup = BeautifulSoup(page, 'html.parser')
+
+
+    @property
+    def quotes(self):
+        locator = QuotesPageLocators.QUOTE
+        quote_tags = self.soup.select(locator) # Retorna uma lista de quotes
+        return [QuoteParser(e) for e in quote_tags]
